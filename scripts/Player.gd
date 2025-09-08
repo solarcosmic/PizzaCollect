1f4e35f722675d2071d944e8bbb9fe771e3c57b1
Solar <abe@solarcosmic.net>
2025-09-08 21:29:24 +1200

    Add Block, Tray & Animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 450.0
 const JUMP_VELOCITY = -400.0
-
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +11,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			animation.play("run")
+		else:
+			animation.play("idle")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
---
+++ scripts/Tray.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+@export var side_offset: Vector2 = Vector2(30, 0)
+@export var idle_offset: Vector2 = Vector2(0, -20)
+@export var smoothness: float = 15.0
+
+func _process(delta: float) -> void:
+	var player = get_parent()
+	if player and player is CharacterBody2D:
+		var target_offset = idle_offset
+		if player.velocity.x > 0:
+			target_offset = side_offset
+		elif player.velocity.x < 0:
+			target_offset = Vector2(-side_offset.x, side_offset.y)
+		position = position.lerp(target_offset, delta * smoothness)
